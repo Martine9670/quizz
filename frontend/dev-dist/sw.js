bdcd80e8f98129fb568557c4676324b42030d4f9
Martine9670 <pinnamartine@free.fr>
2026-02-20 12:26:05 +0100

    modif dyslexic

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-6d1fb45c'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.t5cdacdco7"
+    "revision": "0.ouft0kpj82o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
