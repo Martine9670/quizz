d054751a41459b6707230fce764d7d2f5da38932
Martine9670 <pinnamartine@free.fr>
2026-02-24 15:00:48 +0100

    style: implement neon purple borders for a high-contrast arcade look

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-6d1fb45c'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.arheqbogq6g"
+    "revision": "0.5b3rr3e9da8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
