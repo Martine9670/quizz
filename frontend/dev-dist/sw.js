5dec02b36987e21bf27f9f50095858f014f41319
Martine9670 <pinnamartine@free.fr>
2026-02-25 12:00:03 +0100

    feat: include data import script and update README for easy database seeding

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-6d1fb45c'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.ngpriulngjg"
+    "revision": "0.mtuj4qb98n"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
