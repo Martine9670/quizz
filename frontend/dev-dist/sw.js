148eb40cec4e42f6c3722b2b4f0fd5eea52ce6f3
Martine9670 <pinnamartine@free.fr>
2026-03-09 11:38:21 +0100

    update

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-6d1fb45c'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.c7ekg8bmdro"
+    "revision": "0.tt1hkr22vo4"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
